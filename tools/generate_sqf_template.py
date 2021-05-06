693c8bccf56df7aefe5c7cd5ec2600ec485c2839
JonBons <me@mattgraham.me>
2021-05-06 15:10:08 -0500

    Updated generator script with hardcoded v2.04 commands
    Updated generator script with hardcoded v2.04 commands until I figure out supportInfo or something else

--- tools/generate_sqf_template.py
+++ tools/generate_sqf_template.py
@@ -1,6 +1,7 @@
 import requests
 import re
 import json
+from bs4 import BeautifulSoup
 
 intercept_re = re.compile(r'(get_(binary|unary|nular)_function).*?((?<=\(\").+?(?=\"))')
 intercept_hpp = ""
@@ -25,21 +26,32 @@ control_keywords = [
 ]
 
 constant_keywords = [
-    "nil","true","false",
+    "blufor","civilian","configNull","controlNull",
+    "displayNull","east", "endl","false","grpNull","independent",
+    "lineBreak","locationNull","nil","objNull","opfor","pi","resistance",
+    "scriptNull","sideAmbientLife","sideEmpty","sideLogic","sideUnknown",
+    "taskNull","teamMemberNull","true","west",
     "__EVAL","__EXEC","__FILE__","__LINE__"
 ]
 
 ignored_keywords = list(set(["private","params"] + control_keywords + constant_keywords))
 
-unary_keywords = ["binocularitems","binocularmagazine","compilescript","createhashmapfromarray",
-    "ctrlfontheight","ctrlstyle","ctrltextcolor","ctrltooltip","ctrlurl","fileexists","flatten",
-    "forcecadetdifficulty","forceunicode","getallpylonsinfo","getobjectscale","getplayerid","gettextraw",
-    "keys","markerchannel","markerpolyline","menusetshortcut","menusettext","menuseturl","removeallbinocularitems",
-    "removeallsecondaryweaponitems","removebinocularitem","trim","tvselection","unitcombatmode"]
-binary_keywords = ["addbinocularitem","ctrlsetmouseposition","ctrlseturl","get","getordefault","insert",
-    "isnotequalto","merge","setmarkerpolyline","setmarkerpolylinelocal","setobjectscale","setunitcombatmode",
-    "tvisselected","tvsetselected","tvsortall","tvsortbyvalueall"]
-nular_keywords = ["createhashmap","diag_scope","missionnamesource",""]
+unary_keywords = [
+    "alldiarysubjects","binocularitems","binocularmagazine",
+    "combatbehaviour","compilescript","createhashmapfromarray","ctrlfontheight","ctrlstyle",
+    "ctrltextcolor","ctrltooltip","ctrlurl","diag_localized","fileexists","flatten",
+    "focusedctrl","forcecadetdifficulty","forceunicode","getallpylonsinfo","getobjectscale","getplayerid",
+    "gettextraw","keys","markerchannel","markerpolyline","markershadow","menusetshortcut","menusettext","menuseturl",
+    "opengps","removeallbinocularitems","removeallsecondaryweaponitems","removebinocularitem",
+    "ropesegments","taskname","trim","tvselection","unitcombatmode"
+]
+binary_keywords = ["addbinocularitem","ctrlsetmouseposition","ctrlseturl","fadeEnvironment",
+    "get","getordefault","insert","isnotequalto","merge","setmarkerpolyline",
+    "setmarkerpolylinelocal","setobjectscale","setunitcombatmode",
+    "tvisselected","tvsetselected","tvsortall","tvsortbyvalueall","setCombatBehaviour",
+    "setDiarySubjectPicture","setMarkerShadow","setMarkerShadowLocal","setWeaponZeroing"]
+nular_keywords = ["apertureparams","createhashmap","diag_dumpterrainsynth","diag_scope",
+    "environmentvolume","missionnamesource","speechvolume"]
 
 intercept_commands = intercept_re.finditer(intercept_hpp)
 for m in intercept_commands:
@@ -95,10 +107,33 @@ sqf_out.write(template_text)
 # completions_list = list(set(subl_completions + control_keywords + constant_keywords + unary_keywords + binary_keywords + nular_keywords))
 # completions_list.sort()
 
-# completions_json = {
-#     "scope": "source.sqf",
-#     "completions": completions_list
-# }
+# # completions_json = {
+# #     "scope": "source.sqf",
+# #     "completions": completions_list
+# # }
 
-# completions_out = open("SQF.sublime-completions", 'w')
-# completions_out.write(json.dumps(completions_json))
+# # completions_out = open("SQF.sublime-completions", 'w')
+# # completions_out.write(json.dumps(completions_json))
+
+# current_completions = []
+
+# diff_completions = list(set([x.lower() for x in completions_list]) - set([x.lower() for x in current_completions]))
+
+# correctCommands = []
+# for cmd in diff_completions:
+#     res = requests.get("https://community.bistudio.com/wiki?search=" + cmd + "&title=Special%3ASearch&go=Go")
+#     soup = BeautifulSoup(res.text)
+#     correctName = ""
+#     for lnks in soup.find_all('a'):
+#         if 'title' in lnks.attrs:
+#             if lnks.attrs['title'].lower() == cmd:
+#                 correctName = lnks.text
+#                 break
+
+#     if correctName is not "":
+#         print(correctName)
+#         correctCommands.append(correctName)
+
+# correctCommands.sort()
+# for x in correctCommands:
+#     print('"' + x + '",')
